d511a533de17eed5ab6d6a8e297a8c42d8a30e9b
marco741 <marco741147@gmail.com>
2019-11-06 09:06:52 +0100

    Marco Commit

---
+++ src/oop2019/prova1/gruppo07/Computer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop2019.prova1.gruppo07;
+
+/**
+ *
+ * @author marco
+ */
+public class Computer {
+
+}
