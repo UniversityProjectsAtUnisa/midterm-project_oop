4464345012015271d61279a0a8121ed95af98c2d
fra123456789 <francescocarb98@gmail.com>
2019-11-06 09:18:02 +0100

    interfaccia

---
+++ src/oop2019/prova1/gruppo07/NetworkInterface.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop2019.prova1.gruppo07;
+
+/**
+ *
+ * @author carbo
+ */
+public interface NetworkInterface {
+    public void accept(NetworkInterface sourceInterface, int sourceAddress, int destAddress,String message);
+    public void connect(NetworkInterface other);
+}
